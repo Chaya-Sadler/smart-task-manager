db796c2d380b98feb6dcca7f527046851eb31b24
chaya <chaya.r.sadler@gmail.com>
2025-11-27 14:32:13 -0600

    Add task-service microservice

---
+++ task-service/src/main/java/chaya/sadler/task_service/TaskServiceApplication.java
@@ -0,0 +1,13 @@
+package chaya.sadler.task_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskServiceApplication.class, args);
+	}
+
+}
